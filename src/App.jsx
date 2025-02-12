0fdd7cca7af0614a2529037b6eea0240a5e68ce1
TVimala <vimalareddytummuru@gmail.com>
2025-02-12 22:59:40 +0530

    add:main Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,44 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Main from './components/Main/Main'
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
+import RootLayout from './RootLayout'
+import Faculty from './components/Faculty/Faculty'
+import Admin from './components/Admin/Admin'
+import Login from './components/Login/Login'
+import Rooms from './components/Rooms/Rooms'
+import 'bootstrap/dist/css/bootstrap.min.css';
+const browserRouter = createBrowserRouter([
+  {
+    path: "",
+    element: <RootLayout />,
+    // errorElement: <RoutingError />,
+    children: [
+      {
+        path: "",
+        element:<Main/> 
+      },
+      {
+        path: "/login",
+        element:<Login/>
+      },
+      {
+        path: "/faculty",
+        element:<Faculty /> 
+      },
+      {
+        path: "/rooms",
+        element:<Rooms />
+      },
+      {
+        path: "/admin",
+        element:<Admin /> 
+      },
+    ],
+  },
+]);
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <RouterProvider router={browserRouter} />;
 }
-
 export default App
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Main.css'
+import { Link } from 'react-router-dom'
+function Main() {
+  return (
+      <div className="main d-flex flex-column">
+       <nav>
+      <ul className="nav-links">
+        <li className='faculty me-2'><Link to="/faculty">Faculty</Link></li>
+        <li className='rooms me-2'><Link to="/rooms">Rooms</Link></li>
+        <li className='admin me-2'><Link to="/admin">Admin</Link></li>
+        <li className='login me-2'><Link to="/login">Login</Link></li>
+      </ul>
+    </nav>
+      <h1 className="schedguru">SchedGuru</h1>
+      <blockquote className="blockquote text-end">
+        <p>"Plan your schedule like a pro."</p>
+      </blockquote>
+    </div>
+  )
+}
+
+export default Main

c04bd97748c05533c26b95665005ab1a8f611808
TVimala <vimalareddytummuru@gmail.com>
2025-02-18 23:51:36 +0530

    add:date,rooms

--- src/components/Date/Date.jsx
+++ src/components/Date/Date.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import './Date.css'
 function Date() {
   return (
-    <div>Date</div>
+    <div>
+       <div className="date-period-container">
+          <input type="date" className="date-picker" />
+          <select className="period-selector">
+            <option value="">Select Period</option>
+            {[...Array(7)].map((_, i) => (
+              <option key={i + 1} value={i + 1}>Period {i + 1}</option>
+            ))}
+          </select>
+        </div>
+      </div>
   )
 }
-
 export default Date

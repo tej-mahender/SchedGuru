611c80a1e1bedc5695eb611afaa1b368c77d2f82
Tej <reddytej15@gmail.com>
2025-03-30 13:02:35 +0530

    update:login

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import RootLayout from './RootLayout'
 import Faculty from './components/Faculty/Faculty'
 import Admin from './components/Admin/Admin'
 import SeatingSchedule from './components/SeatingSchedule/SeatingSchedule';
-import Login from './components/login/Login'
+import Login from './components/Login/Login'
 import Date from './components/Date/Date'
 import SeatinPlan from './components/SeatingPlan/SeatingPlan'
 import Profile from './components/Profile/Profile';
